0899da2e8131d9fd147ef389dae29558fcc3a510
MarconiJunior <junior@brumafe.com>
2023-01-04 08:47:12 -0300

    First Commit

---
+++ src/components/Button/styles.jsx
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const ButtonContainer = styled.button`
+    padding: 20px;
+    border: 1px solid #FAFAFA;
+    background-color: #6495ED;
+    color: #FFFFFF;
+    font-size: 24px;
+    font-weight: 700;
+    flex: 1;
+    transition: .35s;
+    
+    &:hover {
+        opacity: 0.6;
+    }
+`
